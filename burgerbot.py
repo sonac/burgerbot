7a89815354523bd6052ec739846ed3d257e3579b
sonac <sonag007@gmail.com>
2021-08-30 23:00:20 +0200

    feat: add caching of messages

--- burgerbot.py
+++ burgerbot.py
@@ -4,6 +4,8 @@ import json
 import threading
 import logging
 import sys
+from dataclasses import dataclass
+from typing import List
 
 import requests
 from telegram.ext import CommandHandler, Updater
@@ -12,8 +14,12 @@ from telegram.ext.callbackcontext import CallbackContext
 from telegram.update import Update
 
 url = 'https://service.berlin.de/terminvereinbarung/termin/tag.php?termin=1&anliegen[]=120686&dienstleisterlist=122210,122217,327316,122219,327312,122227,327314,122231,327346,122243,327348,122252,329742,122260,329745,122262,329748,122254,329751,122271,327278,122273,327274,122277,327276,330436,122280,327294,122282,327290,122284,327292,327539,122291,327270,122285,327266,122286,327264,122296,327268,150230,329760,122301,327282,122297,327286,122294,327284,122312,329763,122314,329775,122304,327330,122311,327334,122309,327332,122281,327352,122279,329772,122276,327324,122274,327326,122267,329766,122246,327318,122251,327320,122257,327322,122208,327298,122226,327300&herkunft=http%3A%2F%2Fservice.berlin.de%2Fdienstleistung%2F120686%2F'
-
 register_prefix = 'https://service.berlin.de'
+
+@dataclass
+class Message:
+  message: str
+  ts: int # timestamp of adding msg to cache in seconds
 
 class Bot:
   def __init__(self) -> None:
@@ -22,6 +28,7 @@ class Bot:
     self.dispatcher = self.bot.dispatcher
     self.dispatcher.add_handler(CommandHandler('start', self.__start))
     self.dispatcher.add_handler(CommandHandler('stop', self.__stop))
+    self.cache: List[Message] = []
 
 
   def __get_chats(self) -> None:
@@ -71,7 +78,7 @@ class Bot:
           logging.warn(e)
       else:
         logging.info("No luck yet")
-      
+      self.__clear_cache()
       time.sleep(15)
 
 
@@ -79,9 +86,24 @@ class Bot:
     self.bot.start_polling()
 
   def __send_message(self, msg: str) -> None:
+    if self.__msg_in_cache(msg):
+      return
+    self.__add_msg_to_cache(msg)
     for c in self.chats:
       self.bot.bot.send_message(chat_id=c, text=msg)
 
+  def __msg_in_cache(self, msg: str) -> bool:
+    for m in self.cache:
+      if m.message == msg:
+        return True
+    return False
+
+  def __add_msg_to_cache(self, msg: str) -> None:
+    self.cache.append(Message(msg, int(time.time())))
+
+  def __clear_cache(self) -> None:
+    cur_ts = int(time.time())
+    self.cache = [m for m in self.cache if (cur_ts - m.ts) > 300]
 
   def start(self) -> None:
     logging.info('Starting Bot')
